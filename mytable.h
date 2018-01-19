4f35bf759f7d34171ac884f3ee4a01dbab203b00
Aswin <aswin6197@gmail.com>
2018-01-20 01:24:21 +0530

    Completed the code for lexical analysis

--- mytable.h
+++ mytable.h
@@ -15,14 +15,19 @@ void prin()
 {
 	printf("1 identifiers \n2 keywords\n3 constants \n4 operators");
 	struct symtab *s = table;
-
-	
+	char map[4][20] = {"Identifier","Keyword","Constants","Operators"};
+	printf("\n-------------------------------------------------------------------------------Symbol Table----------------------------------------------------------------------------------------------");
+	printf("\n\t Name \t\t Value");
 	while(s->next!=NULL){
 	
-		printf("\n name %s value is %d",s->name,s->value);
+		printf("\n\t %s",s->name);
+		printf("%*c", 15-strlen(s->name), ' ');
+		printf("%s",map[s->value-1]);
 		s = s->next;
 	}
-	printf("\nname %s value is %d\n",s->name,s->value);
+	printf("\n\t %s ",s->name);
+	printf("%*c",15-strlen(s->name),' ');
+	printf("%s\n",map[s->value-1]);
 	return;
 }
 
@@ -32,19 +37,17 @@ void insert(char *s,int v){
 	if(p == NULL)	{
 		p = malloc(sizeof(struct symtab));
 		int i;
-//		for (i=0;s[i]!='\0';i++)
-//			p->name[i] = s[i];
 		strcpy(p->name,s);
 		p->value = v;
 		p->next = NULL;
 		table = p;
-		printf("\n%s is first",s);
+//		printf("\n%s is first",s);
 		return;
 	}
 	while(1){
-		printf("hello");
+		
 		if(strcmp(p->name,s)==0){
-			printf("found\n");
+//			printf("found\n");
 			return;
 		}
 		if(p->next == NULL)
@@ -57,7 +60,7 @@ void insert(char *s,int v){
 	temp->value = v;
 	temp->next = NULL;
 	p->next = temp;
-	printf("new %s\n",s);
+//	printf("new %s\n",s);
 
 	return;
 	
